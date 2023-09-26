b865a3c8b7f16d52b53116da5f6084685c39dbb7
wildanjisung <wildanjisung@gmail.com>
2023-09-26 20:19:08 +0700

    add plugin vue router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="redirectTo('/')">navigasi ke home</button>
+    <button @click="toBack">kembali</button>
+    <button @click="toForward">maju</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutView',
+  methods: {
+    redirectTo(path) {
+      this.$router.replace(path)
+    },
+    toBack() {
+      this.$router.go(-1)
+    },
+    toForward() {
+      this.$router.go(1)
+    },
+  }
+}
+</script>
