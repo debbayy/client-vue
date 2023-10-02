8f564b220acb070d0e5d0ef04a427649c68f4635
debyTrisandi <dbts92@gmail.com>
2023-10-02 23:46:17 +0700

    client vue

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,28 +1,63 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/setting">Setting</router-link> |
-      <router-link to="/setting/privacy">Setting - Privacy </router-link> |
-      <router-link to="/user">User</router-link> |
-      <router-link to="/user/123">User Detail</router-link> |
-      <router-link to="/pokemon">Pokemon</router-link>
+  <v-container class="container">
+    <nav class="d-flex flex-row mb-6 cene bg-surface-variant">
+      <v-sheet class="ma-2 pa-2">
+        <router-link class="linkNavbar" to="/">Home</router-link></v-sheet
+      >
+      <v-sheet class="ma-2 pa-2">
+        <router-link to="/about">About</router-link></v-sheet
+      >
+      <v-sheet class="ma-2 pa-2">
+        <router-link to="/setting">Setting</router-link>
+      </v-sheet>
+      <v-sheet class="ma-2 pa-2">
+        <router-link to="/setting/privacy">Setting - Privacy </router-link>
+      </v-sheet>
+      <v-sheet class="ma-2 pa-2">
+        <router-link to="/user">User</router-link>
+      </v-sheet>
+      <v-sheet class="ma-2 pa-2">
+        <router-link to="/user/123">User Detail</router-link>
+      </v-sheet>
+      <v-sheet class="ma-2 pa-2">
+        <router-link to="/pokemon">Pokemon</router-link>
+      </v-sheet>
+      <v-sheet class="ma-2 pa-2">
+        <v-btn @click="changeDark"> Gelap/Terang </v-btn>
+      </v-sheet>
     </nav>
-    <div class="container">
-      <router-view/>
+    <div class="row">
+      <router-view />
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayoutView'
-}
+  name: "DefaultLayoutView",
+  methods: {
+    changeDark() {
+      if (this.$vuetify.theme.dark) {
+        this.$vuetify.theme.themes.light.primary =
+          this.$vuetify.theme.themes.dark.primary;
+      } else {
+        this.$vuetify.theme.themes.dark.primary =
+          this.$vuetify.theme.themes.light.primary;
+      }
+
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .container {
+  min-height: 100vh;
+  width: 100%;
+  /* background-color: whitesmoke; */
+}
+.row {
   margin-left: 50px;
 }
 </style>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,31 +1,47 @@
 <template>
-  <div>
+  <c-container>
     <h1>Halaman Pokemon</h1>
     <h3>list pokemon:</h3>
-    <ul>
-      <li v-for="pokemon in listPokemon" :key="pokemon.name">
+
+    <!-- <li v-for="pokemon in listPokemon" :key="pokemon.name">
         {{ pokemon.name }}
-      </li>
-    </ul>
+      </li> -->
+    <div>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Url</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="pokemon in listPokemon" :key="pokemon.name">
+            <td>{{ pokemon.name }}</td>
+            <td>{{ pokemon.url }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
+
     <button @click="fetchPokemon">fetch Pokemon</button>
-  </div>
+  </c-container>
 </template>
 
 <script>
 export default {
-  name: 'PokemonView',
+  name: "PokemonView",
   computed: {
     listPokemon() {
-      return this.$store.state.pokemon.list
-    }
+      return this.$store.state.pokemon.list;
+    },
   },
   methods: {
     fetchPokemon() {
       this.$store.dispatch("pokemon/fetchList");
-    }
+    },
   },
   mounted() {
-    this.fetchPokemon()
-  }
-}
+    this.fetchPokemon();
+  },
+};
 </script>
